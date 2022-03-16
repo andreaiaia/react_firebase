5a1898caecf9a64f6f6e18a297b38ddcf564c354
Andrea Bianchi <bianan96@gmail.com>
2022-03-16 15:42:39 +0100

    useState lesson

---
+++ chap3/src/App.jsx
@@ -0,0 +1,44 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [showEvents, setShowEvents] = useState(true);
+  const [events, setEvents] = useState([
+    {title: "mario's birthday bash", id: 1},
+    {title: "bowser's live stream", id: 2},
+    {title: "race on moo moo farm", id: 3}
+  ]);
+
+  console.log(showEvents);
+
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      prevEvents.filter((event) => {
+        return id !== event.id;
+    })});
+    console.log(id);
+  }
+
+  return (
+    <div className="App">
+      {showEvents && (
+      <div>
+        <button onClick={() => {setShowEvents(false)}}>Hide Events</button>
+      </div>
+      )}
+      {!showEvents && (
+      <div>
+        <button onClick={() => {setShowEvents(true)}}>Show Events</button>
+      </div>
+      )}
+      {showEvents && events.map((event, index) => (
+        <div key={event.id}>
+          <h2>{index} - {event.title}</h2>
+          <button onClick={() => {handleClick(event.id)}}>Delete Event</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
