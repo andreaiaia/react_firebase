4a411c0c6159a1f1cfe0af48ec291dfee4b37aab
Andrea Bianchi <bianan96@gmail.com>
2022-03-23 11:41:05 +0100

    finish hooks chap

--- tripago/src/hooks/useFetch.jsx
+++ tripago/src/hooks/useFetch.jsx
@@ -1,18 +1,45 @@
 import { useState, useEffect } from "react";
 
-export const useFetch = (url) => {
+export const useFetch = (url, name) => {
     const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(false);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch(url);
-            const json = await response.json();
+        console.log(name);
+        const controller = new AbortController();
 
-            setData(json);
+        const fetchData = async () => {
+            setIsPending(true);
+
+            try {
+                const res = await fetch(url, { signal: controller.signal });
+                if (!res.ok) {
+                    throw new Error(res.statusText);
+                }
+                const json = await res.json();
+    
+                setIsPending(false);
+                setData(json);
+                setError(null);
+            } catch(err) {
+                if (err.name === 'AbortError') {
+                    console.log('Fetch was aborted');
+                } else {
+                    setIsPending(false);
+                    setError('Could not fetch the data');
+                    console.log(err.message);
+                }
+            }
+
         };
 
         fetchData();
-    }, [url]);
 
-    return { data };
+        return () => {
+            controller.abort();
+        };
+    }, [url, name]);
+
+    return { data, isPending, error };
 }
