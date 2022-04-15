9713a5632350d7363258c0f191f744ff97ad74fb
Andrea Bianchi <bianan96@gmail.com>
2022-04-15 10:42:01 +0200

    reset dir

--- multipage-site/src/pages/About.jsx
+++ multipage-site/src/pages/About.jsx
@@ -1,8 +1,8 @@
 export default function About() {
   return (
     <div>
-        <h2>About</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia vero iure rem, culpa aliquam dolore numquam eligendi quo veritatis eaque corrupti odio repellat quisquam, voluptatem, consequuntur illo natus ad blanditiis?</p>
+      <h2>About</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, quisquam. Tempora quod dolores a, at quis accusantium praesentium vitae beatae aliquam harum autem fugit! Unde repellendus est pariatur quia odit!</p>
     </div>
   )
 }
--- multipage-site/src/pages/Contact.jsx
+++ multipage-site/src/pages/Contact.jsx
@@ -1,8 +1,8 @@
 export default function Contact() {
   return (
     <div>
-        <h2>Contact</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia vero iure rem, culpa aliquam dolore numquam eligendi quo veritatis eaque corrupti odio repellat quisquam, voluptatem, consequuntur illo natus ad blanditiis?</p>
+      <h2>Contact</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam explicabo laudantium nemo voluptas cum omnis error voluptate. Nihil numquam ipsum necessitatibus hic odit neque consequuntur dolor. Magni quos ratione iste.</p>
     </div>
   )
 }
